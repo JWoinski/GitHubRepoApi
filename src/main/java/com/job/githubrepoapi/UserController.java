873e562ae8b683756997c123312550933770053a
jakub <jakub.woinski@icloud.com>
2024-08-13 13:37:40 +0200

    #124 Task from employer.

--- src/main/java/com/job/githubrepoapi/UserController.java
+++ src/main/java/com/job/githubrepoapi/UserController.java
@@ -2,6 +2,7 @@ package com.job.githubrepoapi;
 
 import com.job.githubrepoapi.service.GitHubService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +18,15 @@ public class UserController {
     private final GitHubService gitHubService;
 
     @GetMapping("/{login}")
-    public ResponseEntity<List<Map<String, Object>>> getRepositories(@PathVariable String login,
-                                                                     @RequestHeader("Accept") String acceptHeader) {
-        return new ResponseEntity<>(gitHubService.getRepositories(login), HttpStatus.OK);
+    public ResponseEntity<List<Map<String, Object>>> getRepositories(
+            @PathVariable String login,
+            @RequestHeader(value = HttpHeaders.ACCEPT, defaultValue = "application/json") String acceptHeader) {
+
+        if (!"application/json".equalsIgnoreCase(acceptHeader)) {
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        List<Map<String, Object>> repositories = gitHubService.getRepositories(login);
+        return new ResponseEntity<>(repositories, HttpStatus.OK);
     }
 }
